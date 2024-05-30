0fbafcc73203cd27deb4a50d1d72c834f18643de
smahlaba023 <smahlaba023@student.wethinkcode.co.za>
2024-05-30 08:31:15 +0200

    templates

---
+++ ArithmeticCalculator/src/main/java/basic/calculator/arithmetic/Calculator.java
@@ -0,0 +1,29 @@
+package basic.calculator.arithmetic;
+
+public class Calculator {
+
+    // 1. Addition method.
+    public static long addition() {
+        // TODO
+        return 1;
+    }
+
+    // 2. Subtraction method.
+    public static long subtraction() {
+        // TODO
+        return 1;
+    }
+
+    // 3. Multiplication method.
+    public static double multiplication() {
+        // TODO
+        return 1.0;
+    }
+
+    // 4. Division method.
+    public static double division() {
+        // TODO
+        return 1.0;
+    }
+
+}
---
+++ ArithmeticCalculator/src/test/java/basic/calculator/arithmetic/CalculatorTest.java
@@ -0,0 +1,26 @@
+package basic.calculator.arithmetic;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void additionTest() {
+        // TODO
+    }
+
+    @Test
+    public void subtractionTest() {
+        // TODO
+    }
+
+    @Test
+    public void multiplicationTest() {
+
+    }
+
+    @Test
+    public void divisionTest() {
+
+    }
+}
