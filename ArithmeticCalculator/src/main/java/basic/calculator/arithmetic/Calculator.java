7c11f6bb2c4b64ad1e72cfb19d4b8ff6f1371d96
smahlaba023 <smahlaba023@student.wethinkcode.co.za>
2024-05-31 13:09:10 +0200

    updated addition & added new methods

--- ArithmeticCalculator/src/main/java/basic/calculator/arithmetic/Calculator.java
+++ ArithmeticCalculator/src/main/java/basic/calculator/arithmetic/Calculator.java
@@ -1,15 +1,23 @@
 package basic.calculator.arithmetic;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Calculator {
+    private static ArrayList<Integer> numbersArray = new ArrayList<>();
 
     // 1. Addition method.
-    public static long addition() {
-        // TODO
-        return 1;
+    public static int addition() {
+        int sum = 0;
+        for (int number: numbersArray) {
+            sum += number;
+        }
+        return sum;
     }
 
     // 2. Subtraction method.
-    public static long subtraction() {
+    public static int subtraction() {
         // TODO
         return 1;
     }
@@ -26,4 +34,39 @@ public class Calculator {
         return 1.0;
     }
 
+    public static void input() {
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Enter the numbers you wish to add/subtract/multiply/divide.\nEnter a \"done\" when finished.");
+
+        while (true) {
+            System.out.print("Enter the number: ");
+            String number = userInput.nextLine();
+
+            if (number.equalsIgnoreCase("Done")) {
+                break;
+            } else if (isNumber(number)) {
+                numbersArray.add(Integer.parseInt(number));
+            }
+        }
+    }
+
+    public static boolean isNumber(String userInput) {
+
+        // try/catch block to try change a string input by the user to an int type.
+        // and catch a NumberFormatException if it cannot become an int.
+        try {
+            Integer.parseInt(userInput);
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.print("Invalid input. ");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        System.out.println(addition());
+    }
 }
