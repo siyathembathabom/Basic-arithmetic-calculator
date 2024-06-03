3b77fb29b21c755e4ca9d2e7b348604f3dbe57e4
smahlaba023 <smahlaba023@student.wethinkcode.co.za>
2024-06-03 12:56:34 +0200

    updated all operations

--- ArithmeticCalculator/src/main/java/basic/calculator/arithmetic/Calculator.java
+++ ArithmeticCalculator/src/main/java/basic/calculator/arithmetic/Calculator.java
@@ -1,14 +1,14 @@
 package basic.calculator.arithmetic;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calculator {
     private static final ArrayList<Integer> numbersArray = new ArrayList<>();
+    private static final Scanner userInput = new Scanner(System.in);
 
     // 1. Addition method.
-    public static int addition() {
+    private static int addition() {
         int sum = 0;
         for (int number: numbersArray) {
             sum += number;
@@ -18,32 +18,42 @@ public class Calculator {
     }
 
     // 2. Subtraction method.
-    public static int subtraction() {
-        // TODO
-        return 1;
+    private static int subtraction() {
+        int difference = numbersArray.getFirst();
+
+        for (int i = 1; i < numbersArray.size(); i++) {
+            difference -= numbersArray.get(i);
+        }
+        numbersArray.clear();
+        return difference;
     }
 
     // 3. Multiplication method.
-    public static double multiplication() {
-        // TODO
-        return 1.0;
+    private static double multiplication() {
+        int product = 1;
+
+        for (int number: numbersArray) {
+            product *= number;
+        }
+        numbersArray.clear();
+        return product;
     }
 
     // 4. Division method.
-    public static double division() {
-        // TODO
-        return 1.0;
+    private static double division() {
+        double quotient = numbersArray.getFirst();
+
+        for (int i = 1; i < numbersArray.size(); i++) {
+            quotient /= numbersArray.get(i);
+        }
+        numbersArray.clear();
+        return quotient;
     }
 
-
-    public static void input() {
-
-        Scanner userInput = new Scanner(System.in);
-
-        System.out.println("Enter the numbers you wish to add/subtract/multiply/divide.\nEnter a \"done\" when finished.");
+    private static void numberInput() {
 
         while (true) {
-            System.out.print("Enter the number: ");
+            System.out.print("Enter the number or done: ");
             String number = userInput.nextLine();
 
             if (number.equalsIgnoreCase("Done")) {
@@ -54,7 +64,34 @@ public class Calculator {
         }
     }
 
-    public static boolean isNumber(String userInput) {
+    private static String askForType() {
+        System.out.println("\nThere are 4 types of operations: add (+), subtract (-), multiply (*), divide (/)");
+
+        while (true) {
+            System.out.print("Enter the type of operation you want: ");
+            String theType = userInput.nextLine();
+
+            switch (theType) {
+                case "add", "+", "subtract", "-", "multiply", "*", "divide", "/":
+                    return theType;
+            }
+        }
+    }
+
+    private static void type(String type) {
+
+        if (type.equalsIgnoreCase("add") || type.equalsIgnoreCase("+")) {
+            System.out.println(addition());
+        } else if (type.equalsIgnoreCase("subtract") || type.equalsIgnoreCase("-")) {
+            System.out.println(subtraction());;
+        } else if (type.equalsIgnoreCase("multiply") || type.equalsIgnoreCase("*")) {
+            System.out.println(multiplication());
+        } else if (type.equalsIgnoreCase("divide") || type.equalsIgnoreCase("/")) {
+            System.out.println(division());
+        }
+    }
+
+    private static boolean isNumber(String userInput) {
 
         // try/catch block to try change a string input by the user to an int type.
         // and catch a NumberFormatException if it cannot become an int.
@@ -67,8 +104,15 @@ public class Calculator {
         }
     }
 
+    private static void welcome() {
+        System.out.println("This is a basic arithmetic calculator.");
+        System.out.println("Enter the numbers you wish to add/subtract/multiply/divide.\nEnter \"done\" when finished.");
+        System.out.println("Note the operations occur by the sequence of numbers you add.\n*** If you enter 1 thereafter 2, and you decide to divide it, it will be 1 / 2 resulting 0.5. ***\n");
+    }
+
     public static void main(String[] args) {
-        input();
-        System.out.println(addition());
+        welcome();
+        numberInput();
+        type(askForType());
     }
 }
