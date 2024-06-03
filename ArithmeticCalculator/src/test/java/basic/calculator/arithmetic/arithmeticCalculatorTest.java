c7edbc94794e43581da2ba9facf85421a6fdc1e2
siyathembathabom <siyathembathabom@duck.com>
2024-06-03 22:47:36 +0200

    Added operator methoods tests

--- ArithmeticCalculator/src/test/java/basic/calculator/arithmetic/arithmeticCalculatorTest.java
+++ ArithmeticCalculator/src/test/java/basic/calculator/arithmetic/arithmeticCalculatorTest.java
@@ -1,26 +1,56 @@
 package basic.calculator.arithmetic;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class arithmeticCalculatorTest {
 
     @Test
     public void additionTest() {
-        // TODO
+        ArrayList<Integer> numbersList = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            numbersList.add(i);
+        }
+        int sum = arithmeticCalculator.addition(numbersList);
+        assertEquals(10, sum);
     }
 
     @Test
     public void subtractionTest() {
-        // TODO
+        ArrayList<Integer> numbersList = new ArrayList<>();
+
+        for (int i = 2; i >= 1; i--) {
+            numbersList.add(i);
+        }
+        int difference = arithmeticCalculator.subtraction(numbersList);
+        assertEquals(1, difference);
     }
 
     @Test
     public void multiplicationTest() {
-        // TODO
+        ArrayList<Integer> numbersList = new ArrayList<>();
+
+        for (int i = 1; i < 5; i++) {
+            numbersList.add(i);
+        }
+        int product = (int) arithmeticCalculator.multiplication(numbersList);
+        assertEquals(24, product);
     }
+
 
     @Test
     public void divisionTest() {
-        // TODO
+        ArrayList<Integer> numbersList = new ArrayList<>();
+
+        for (int i = 1; i <= 2; i++) {
+            numbersList.add(i);
+        }
+        double quotient = arithmeticCalculator.division(numbersList);
+        double delta = 0.0001; // Set an acceptable difference/delta
+        assertEquals(0.5, quotient, delta);
     }
 }
